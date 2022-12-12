5457198a9a8f58980797a83449985c58f797297a
Chetan Sharma <cschetan559@gmail.com>
2022-12-12 19:40:03 +0530

    started  learning java

--- FirstClass.java
+++
@@ -1,7 +0,0 @@
-// 06-12-2022 ->Started learning Java
-
-class FirstClass {
-     public static void main(String[] args) {
-        System.out.println("Heyy This IS My First Java Program!!"); 
-    }
-}
---
+++ JAVA/FirstClass.java
@@ -0,0 +1,13 @@
+package JAVA;
+// 06-12-2022 ->Started learning Java
+
+class FirstClass {
+     public static void main(String[] args) {
+        System.out.println("Heyy This IS My First Java Program!!\nHere are some star pattern");
+        System.out.println("*"); 
+        System.out.println("**"); 
+        System.out.println("***"); 
+        System.out.println("****"); 
+        System.out.println("*****"); 
+    }
+}
